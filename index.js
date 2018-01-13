81ee23b7153106cd01f5fb580bab61607175f949
Louise Johnson <louisemaejohnson@gmail.com>
2018-01-13 04:26:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
     
     itemsAndPrices.push(`${item} at $${price}` + (i === cart.length-1? '.':', '));
   }
-  console.log(${itemsAndPrices});
+  console.log(`${itemsAndPrices}`);
 }
 
 function total() {
